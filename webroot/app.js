dd723eac0550dce442967ad3a76533ca9fe3575f
nasonfish <nasonfish@gmail.com>
2013-08-22 12:48:17 -0600

    Make animations happen for the things we use javascript for so far - edit.php, showing a full tutorial on the front page

--- webroot/app.js
+++ webroot/app.js
@@ -1,14 +1,23 @@
 $('.showall').click(function(){
     $('#' + $(this).attr('for') + '-dot').hide();
-    $('#' + $(this).attr('for')).slideDown();
+    $('#' + $(this).attr('for')).slideDown("slow");
     $(this).hide();
 });
 
 $('.markdown-test').click(function(){
     var elem = $(this);
-    $('#md-preview').remove();
-    $.post('ajax_markdown.php', {text: $('#text').val()})
-    .done(function(data){
-        elem.after('<div id="md-preview">' + data + '</div>');
-    });
+    var old = $('#md-preview');
+    var displayNew = function(){
+        $.post('ajax_markdown.php', {text: $('#text').val()})
+            .done(function(data){
+                elem.after('<div id="md-preview" style="display: none;">' + data + '</div>');
+                $('#md-preview').slideDown("slow");
+                old.remove();
+            });
+    };
+    if(old.length != 0){ // Check if there are any md-preview elements on the page.
+        old.slideUp("slow", displayNew);
+    } else {
+        displayNew();
+    }
 });
