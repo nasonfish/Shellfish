293e64e97d3957ce6147e7739982f1f9c0088805
nasonfish <nasonfish@gmail.com>
2013-09-12 03:56:22 -0600

    Actually, redirect the /r/ page to the /404/ page instead of printing stuff - that's not very friendly.

--- webroot/r.php
+++ webroot/r.php
@@ -1,10 +1,10 @@
 <?php
 if(isset($args[0])){
-$id = $args[0];
-include('../libs/Tutorials.class.php');
-$tutorials = new Tutorials;
-$page = $tutorials->page($id);
-header(sprintf('Location: /tutorial/%s/%s/', $page->getTitleSlug(), $id));
+    $id = $args[0];
+    include('../libs/Tutorials.class.php');
+    $tutorials = new Tutorials;
+    $page = $tutorials->page($id);
+    header(sprintf('Location: /tutorial/%s/%s/', $page->getTitleSlug(), $id));
 } else {
-    echo "I found no place to redirect you to.";
+    header('Location: /404/');
 }
