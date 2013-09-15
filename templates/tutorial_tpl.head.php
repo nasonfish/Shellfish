1052f66927fe0743a0933951a56059e1d7e3f11d
nasonfish <nasonfish@gmail.com>
2013-09-15 14:19:31 -0600

    Changes to styles for clicking on tutorials, added canonical link to the pages for SEO

--- templates/tutorial_tpl.head.php
+++ templates/tutorial_tpl.head.php
@@ -1,6 +1,11 @@
 <?php
 $pass[1] = isset($pass[1]) ? $pass[1] : -1;
-$page = $tutorials->page($pass[1]);
-if($pass[0] != ($slug = $page->getTitleSlug())){
-    echo '<link rel="canonical" href="/tutorial/'.$slug.'/'.$page->getId().'/" />';
+try {
+    $page = $tutorials->page($pass[1]);
+    if($pass[0] != ($slug = $page->getTitleSlug())){
+        echo '<link rel="canonical" href="/tutorial/'.$slug.'/'.$page->getId().'/" />';
+    }
+    $page->view();
+} catch (PredisPageDoesNotExistException $e){
+
 }
