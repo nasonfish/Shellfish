0a76daafd5b8fc61f0689d2011001f1151c379f0
nasonfish <nasonfish@gmail.com>
2013-09-23 22:38:40 -0600

    Fixed and moved the canonical link

--- templates/tutorial_tpl.css.php
+++ templates/tutorial_tpl.css.php
@@ -1,8 +1,8 @@
 <?php
-$pass[1] = isset($pass[1]) ? $pass[1] : -1;
+$pass[0] = isset($pass[0]) ? $pass[0] : -1;
 try {
-    $page = $tutorials->page($pass[1]);
-    if($pass[0] != ($slug = $page->getTitleSlug())){
+    $page = $tutorials->page($pass[0]);
+    if($pass[1] != ($slug = $page->getTitleSlug())){
         echo '<link rel="canonical" href="/tutorial/'.$page->getId().'/'.$slug.'/" />';
     }
     $page->view();
