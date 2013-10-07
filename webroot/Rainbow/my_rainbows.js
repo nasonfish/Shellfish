3cf211a53ea02e5ad2fbc9a7577c2258ca36cc8d
nasonfish <nasonfish@gmail.com>
2013-10-06 22:45:09 -0600

    More messing around with Rainbows and Terminal syntax highlighting

--- webroot/Rainbow/my_rainbows.js
+++ webroot/Rainbow/my_rainbows.js
@@ -1,16 +1,26 @@
 Rainbow.extend('terminal', [
     {
+        'name': 'superuser',
+        'pattern': /root|sudo/g
+    },
+    {
+        'matches': {
+            1: 'package-manager.command',
+            2: 'package-manager.subcommand',
+            3: 'package-manager.package'
+        },
+        'pattern': /(apt\-get|yum|aptitude) (install) ([^ ]+)|(pacman) (\-S) ([^ ]+)/g
+    },
+    {
         'matches': {
             1: 'shellpart.user',
             2: 'shellpart.at',
             3: 'shellpart.host',
             4: 'shellpart.colon',
             5: 'shellpart.directory',
-            6: 'shellpart.prompt',
-            7: 'command',
-            8: 'subcommand'
+            6: 'shellpart.prompt'
         },
-        'pattern': /([^@]+)(@)([^:]+)(:)([^#\$]+)([#\$]? *)([^ ]*) *(.*$)/g
+        'pattern': /([^@]+)(@)([^:]+)(:)([^#\$]+)([#\$]? )/g
     }
 ]);
 Rainbow.color();
