5aa72c939df333c6441b5e29a964b5be0daaa594
Daniel Barnes <nasonfish@nasonfish.com>
2014-10-13 00:34:56 -0600

    don't process our special syntax handles on the truncated text, just remove them

--- libs/Tutorials.class.php
+++ libs/Tutorials.class.php
@@ -128,7 +128,7 @@ class Tutorials {
             '%title%' => htmlspecialchars($tutorial->getTitle()),
             '%desc%' => htmlspecialchars($tutorial->getDescription()),
             '%user%' => htmlspecialchars($tutorial->getUsername()),
-            '%ttext%' => $this->md->defaultTransform(preg_replace("/(?<=.{200,300})</pre>.*$|(?<=.{300}).*$/s", "", $this->syntax($text))),
+            '%ttext%' => preg_replace("/[{}]/g", "", $this->md->defaultTransform(strlen($text) > 250 ? substr($text, 0, 250) . '...' : $text)),
             '%ftext%' => $this->md->defaultTransform($this->syntax($text)),
             '%category%' => ucwords(htmlspecialchars($category)),
             '%categorylink%' => '/category/' . str_replace(' ', '+', ucwords(htmlspecialchars($category))) . '/',
