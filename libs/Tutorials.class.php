c9d08e3a47a733c050de5c6870ebb7703287369a
Daniel Barnes <nasonfish@nasonfish.com>
2014-10-13 00:31:10 -0600

    do better checks on truncating, rather than splitting and then working around it

--- libs/Tutorials.class.php
+++ libs/Tutorials.class.php
@@ -128,7 +128,7 @@ class Tutorials {
             '%title%' => htmlspecialchars($tutorial->getTitle()),
             '%desc%' => htmlspecialchars($tutorial->getDescription()),
             '%user%' => htmlspecialchars($tutorial->getUsername()),
-            '%ttext%' => preg_replace("/&lt;</p>\n+<p>[^>]+>/sg", "", $this->md->defaultTransform($this->syntax(strlen($text) > 250 ? substr($text, 0, 250) . '...' : $text))),
+            '%ttext%' => $this->md->defaultTransform(preg_replace("/(?<=.{200,300})</pre>.*$|(?<=.{300}).*$/s", "", $this->syntax($text))),
             '%ftext%' => $this->md->defaultTransform($this->syntax($text)),
             '%category%' => ucwords(htmlspecialchars($category)),
             '%categorylink%' => '/category/' . str_replace(' ', '+', ucwords(htmlspecialchars($category))) . '/',
